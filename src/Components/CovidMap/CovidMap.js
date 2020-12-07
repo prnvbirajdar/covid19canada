8534479691a66abd904a38a2f5fc0b518fab1b52
prnvbirajdar <prnvbirajdar@gmail.com>
2020-12-07 03:32:49 -0500

    refactored table to map everything from the main object

--- src/Components/CovidMap/CovidMap.js
+++ src/Components/CovidMap/CovidMap.js
@@ -2,26 +2,46 @@ import React, {useState} from 'react'
 import {mapboxKey} from '../Api/Api'
 import {Popup, TileLayer, MapContainer, Marker, GeoJSON} from 'react-leaflet'
 import {Icon} from 'leaflet'
-import canadaData from '../../Data/canada.geo.json'
+import canadaData from '../../Data/newCanada.geo.json'
 import './covid-map.css'
 import ReactMapGL from 'react-map-gl'
 
 function CovidMap() {
-    const [viewport, setViewport] = useState({
-        latitude: 58.859224,
-        longitude: -96.260852,
-        zoom: 3,
-        width:"100%",
-        height:"60vh"
-    })
+    // const [viewport, setViewport] = useState({
+    //     latitude: 58.859224,
+    //     longitude: -96.260852,
+    //     zoom: 3,
+    //     width:"100%",
+    //     height:"60vh"
+    // })
 
     const canadaStyle = {
         color:'#141414',
-        weight:1
+        weight:1,
+        fillColor:'purple',
+        fillOpacity:0.5
     }
 
-    const onEachProvince = (province, layer)=>{
+    console.log(canadaData)
 
+    const onEachProvince = (province, layer)=>{
+        const provinceName = province.properties.NAME
+        layer.bindPopup(provinceName)
+
+        layer.on({
+            mouseover: (event)=>{
+                layer.openTooltip()
+               event.target.setStyle({
+                   fillColor: 'yellow',
+               })
+            },
+            mouseout: (event)=>{
+                layer.closeTooltip()
+               event.target.setStyle({
+                   fillColor: 'purple',
+               })
+            },
+        })
     }
 
     return (
@@ -33,16 +53,20 @@ function CovidMap() {
 
 
 
-            <ReactMapGL 
-                {...viewport} 
-                mapboxApiAccessToken={mapboxKey}
-                onViewportChange={(viewport)=>setViewport(viewport)}
-                mapStyle="mapbox://styles/cg709/cki6bfur32hye19o47s7r367b"></ReactMapGL>
+           
         </React.Fragment>
     )
 }
 
 export default CovidMap
+
+
+
+// <ReactMapGL 
+// {...viewport} 
+// mapboxApiAccessToken={mapboxKey}
+// onViewportChange={(viewport)=>setViewport(viewport)}
+// mapStyle="mapbox://styles/cg709/cki6bfur32hye19o47s7r367b"></ReactMapGL>
 
 // <TileLayer
 //                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
--- src/Containers/Table/Table.js
+++ src/Containers/Table/Table.js
@@ -1,45 +1,14 @@
 import React from 'react'
+import { provinces} from '../../Components/Api/Api'
+
 
 function Table({ basicData}) {
 
-    const AB = basicData[0].data.data
-    const lastAB = AB[AB.length -1]
+    const lData = basicData.map((e)=>e.data.data)
 
-    const BC = basicData[1].data.data
-    const lastBC = BC[BC.length -1]
+    let lastData = lData.map((e)=>e[e.length -1])
 
-    const MB = basicData[2].data.data
-    const lastMB = MB[MB.length -1]
-
-    const NB = basicData[3].data.data
-    const lastNB = NB[NB.length -1]
-
-    const NL = basicData[4].data.data
-    const lastNL = NL[NL.length -1]
-
-    const NS = basicData[5].data.data
-    const lastNS = NS[NS.length -1]
-
-    const NT = basicData[6].data.data
-    const lastNT = NT[NT.length -1]
-
-    const NU = basicData[7].data.data
-    const lastNU = NU[NU.length -1]
-
-    const ON = basicData[8].data.data
-    const lastON = ON[ON.length -1]
-
-    const PE = basicData[9].data.data
-    const lastPE = PE[PE.length -1]
-
-    const QC = basicData[10].data.data
-    const lastQC = QC[QC.length -1]
-
-    const SK = basicData[11].data.data
-    const lastSK = SK[SK.length -1]
-
-    const YT = basicData[12].data.data
-    const lastYT = YT[YT.length -1]
+    let newData = lastData.map((e,i)=>e['name'] = (provinces[i].Name))
 
     return (
 
@@ -55,124 +24,20 @@ function Table({ basicData}) {
                         <th>Tests</th>
                     </tr>
                 </thead>
-                <tbody>    
-                    <tr>
-                        <td data-label="Province">Alberta</td>
-                        <td data-label="Cases">{lastAB.total_cases}</td>
-                        <td data-label="Deaths">{lastAB.total_fatalities}</td>
-                        <td data-label="Active">{lastAB.total_cases - lastAB.total_recoveries - lastAB.total_fatalities}</td>
-                        <td data-label="Recoveries">{lastAB.total_recoveries}</td>
-                        <td data-label="Hospitalizations">{lastAB.total_hospitalizations}</td>
-                        <td data-label="Cases">{lastAB.total_tests}</td>
-                    </tr>
-                    <tr>
-                        <td data-label="Province">British Columbia</td>
-                        <td data-label="Cases">{lastBC.total_cases}</td>
-                        <td data-label="Deaths">{lastBC.total_fatalities}</td>
-                        <td data-label="Active">{lastBC.total_cases - lastBC.total_recoveries - lastBC.total_fatalities}</td>
-                        <td data-label="Recoveries">{lastBC.total_recoveries}</td>
-                        <td data-label="Hospitalizations">{lastBC.total_hospitalizations}</td>
-                        <td data-label="Cases">{lastBC.total_tests}</td>
-                    </tr>
-                    <tr>
-                        <td data-label="Province">Manitoba</td>
-                        <td data-label="Cases">{lastMB.total_cases}</td>
-                        <td data-label="Deaths">{lastMB.total_fatalities}</td>
-                        <td data-label="Active">{lastMB.total_cases - lastMB.total_recoveries - lastMB.total_fatalities}</td>
-                        <td data-label="Recoveries">{lastMB.total_recoveries}</td>
-                        <td data-label="Hospitalizations">{lastMB.total_hospitalizations}</td>
-                        <td data-label="Cases">{lastMB.total_tests}</td>
-                    </tr>
-                    <tr>
-                        <td data-label="Province">New Brunswick</td>
-                        <td data-label="Cases">{lastNB.total_cases}</td>
-                        <td data-label="Deaths">{lastNB.total_fatalities}</td>
-                        <td data-label="Active">{lastNB.total_cases - lastNB.total_recoveries - lastNB.total_fatalities}</td>
-                        <td data-label="Recoveries">{lastNB.total_recoveries}</td>
-                        <td data-label="Hospitalizations">{lastNB.total_hospitalizations}</td>
-                        <td data-label="Cases">{lastNB.total_tests}</td>
-                    </tr>
-                    <tr>
-                        <td data-label="Province">Newfoundland and Labrador</td>
-                        <td data-label="Cases">{lastNL.total_cases}</td>
-                        <td data-label="Deaths">{lastNL.total_fatalities}</td>
-                        <td data-label="Active">{lastNL.total_cases - lastNL.total_recoveries - lastNL.total_fatalities}</td>
-                        <td data-label="Recoveries">{lastNL.total_recoveries}</td>
-                        <td data-label="Hospitalizations">{lastNL.total_hospitalizations}</td>
-                        <td data-label="Cases">{lastNL.total_tests}</td>
-                    </tr>
-                    <tr>
-                        <td data-label="Province">Nova Scotia</td>
-                        <td data-label="Cases">{lastNS.total_cases}</td>
-                        <td data-label="Deaths">{lastNS.total_fatalities}</td>
-                        <td data-label="Active">{lastNS.total_cases - lastNS.total_recoveries - lastNS.total_fatalities}</td>
-                        <td data-label="Recoveries">{lastNS.total_recoveries}</td>
-                        <td data-label="Hospitalizations">{lastNS.total_hospitalizations}</td>
-                        <td data-label="Cases">{lastNS.total_tests}</td>
-                    </tr>
-                    <tr>
-                        <td data-label="Province">Northwest Territories</td>
-                        <td data-label="Cases">{lastNT.total_cases}</td>
-                        <td data-label="Deaths">{lastNT.total_fatalities}</td>
-                        <td data-label="Active">{lastNT.total_cases - lastNT.total_recoveries - lastNT.total_fatalities}</td>
-                        <td data-label="Recoveries">{lastNT.total_recoveries}</td>
-                        <td data-label="Hospitalizations">{lastNT.total_hospitalizations}</td>
-                        <td data-label="Cases">{lastNT.total_tests}</td>
-                    </tr>
-                    <tr>
-                        <td data-label="Province">Nunavat</td>
-                        <td data-label="Cases">{lastNU.total_cases}</td>
-                        <td data-label="Deaths">{lastNU.total_fatalities}</td>
-                        <td data-label="Active">{lastNU.total_cases - lastNU.total_recoveries - lastNU.total_fatalities}</td>
-                        <td data-label="Recoveries">{lastNU.total_recoveries}</td>
-                        <td data-label="Hospitalizations">{lastNU.total_hospitalizations}</td>
-                        <td data-label="Cases">{lastNU.total_tests}</td>
-                    </tr>
-                    <tr>
-                        <td data-label="Province">Ontario</td>
-                        <td data-label="Cases">{lastON.total_cases}</td>
-                        <td data-label="Deaths">{lastON.total_fatalities}</td>
-                        <td data-label="Active">{lastON.total_cases - lastON.total_recoveries - lastON.total_fatalities}</td>
-                        <td data-label="Recoveries">{lastON.total_recoveries}</td>
-                        <td data-label="Hospitalizations">{lastON.total_hospitalizations}</td>
-                        <td data-label="Cases">{lastON.total_tests}</td>
-                    </tr>
-                    <tr>
-                        <td data-label="Province">Prince Edward Island</td>
-                        <td data-label="Cases">{lastPE.total_cases}</td>
-                        <td data-label="Deaths">{lastPE.total_fatalities}</td>
-                        <td data-label="Active">{lastPE.total_cases - lastPE.total_recoveries - lastPE.total_fatalities}</td>
-                        <td data-label="Recoveries">{lastPE.total_recoveries}</td>
-                        <td data-label="Hospitalizations">{lastPE.total_hospitalizations}</td>
-                        <td data-label="Cases">{lastPE.total_tests}</td>
-                    </tr>
-                    <tr>
-                        <td data-label="Province">Quebec</td>
-                        <td data-label="Cases">{lastQC.total_cases}</td>
-                        <td data-label="Deaths">{lastQC.total_fatalities}</td>
-                        <td data-label="Active">{lastQC.total_cases - lastQC.total_recoveries - lastQC.total_fatalities}</td>
-                        <td data-label="Recoveries">{lastQC.total_recoveries}</td>
-                        <td data-label="Hospitalizations">{lastQC.total_hospitalizations}</td>
-                        <td data-label="Cases">{lastQC.total_tests}</td>
-                    </tr>                    
-                    <tr>
-                        <td data-label="Province">Saskatchewan</td>
-                        <td data-label="Cases">{lastSK.total_cases}</td>
-                        <td data-label="Deaths">{lastSK.total_fatalities}</td>
-                        <td data-label="Active">{lastSK.total_cases - lastSK.total_recoveries - lastSK.total_fatalities}</td>
-                        <td data-label="Recoveries">{lastSK.total_recoveries}</td>
-                        <td data-label="Hospitalizations">{lastSK.total_hospitalizations}</td>
-                        <td data-label="Cases">{lastSK.total_tests}</td>
-                    </tr> 
-                    <tr>
-                        <td data-label="Province">Yukon</td>
-                        <td data-label="Cases">{lastYT.total_cases}</td>
-                        <td data-label="Deaths">{lastYT.total_fatalities}</td>
-                        <td data-label="Active">{lastYT.total_cases - lastYT.total_recoveries - lastYT.total_fatalities}</td>
-                        <td data-label="Recoveries">{lastYT.total_recoveries}</td>
-                        <td data-label="Hospitalizations">{lastYT.total_hospitalizations}</td>
-                        <td data-label="Cases">{lastYT.total_tests}</td>
-                    </tr> 
+                <tbody>  
+                    {lastData.map(data=>{
+                        return(
+                            <tr>
+                                <td data-label="Province">{data.name}</td>
+                                <td data-label="Cases">{data.total_cases}</td>
+                                <td data-label="Deaths">{data.total_fatalities}</td>
+                                <td data-label="Active">{data.total_cases - data.total_recoveries - data.total_fatalities}</td>
+                                <td data-label="Recoveries">{data.total_recoveries}</td>
+                                <td data-label="Hospitalizations">{data.total_hospitalizations}</td>
+                                <td data-label="Cases">{data.total_tests}</td>
+                            </tr>  
+                        )
+                    })}
                 </tbody>
             </table>
     )
