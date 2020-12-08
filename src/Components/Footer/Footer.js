3423e06be2fc9c8de91f9b89d0e1d557677039e5
prnvbirajdar <prnvbirajdar@gmail.com>
2020-12-08 01:54:11 -0500

    added footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,10 +1,46 @@
 import React from 'react'
+import './footer.css'
 
 function Footer() {
     return (
-        <div>
-            Footer
-        </div>
+        <footer className="footer">
+            <ul className="social">
+                <li className="social-list">
+                    <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href="mailto: prnvbirajdar@gmail.com"
+                    className="social-link">
+                        <i className="far fa-envelope"></i>
+                    </a>
+                </li>
+                <li className="social-list">
+                    <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://github.com/prnvbirajdar"
+                    className="social-link">
+                        <i className="fab fa-github-alt"></i>
+                    </a>
+                </li>
+                <li className="social-list">
+                    <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://www.linkedin.com/in/prnvbirajdar/"
+                    className="social-link">
+                        <i className="fab fa-linkedin-in"></i>
+                    </a>
+                </li>
+            </ul>
+            <div className="copyright__div">
+                <p className="copyright__p">
+                    <a className="copyright" href="#top">©2020 Made with ❤️ by Pranav Birajdar</a>
+                </p>
+            </div>
+      </footer>
+     
+     
     )
 }
 
