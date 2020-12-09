91e230bf973a556fa890b9b55e10bf609bb91a25
prnvbirajdar <prnvbirajdar@gmail.com>
2020-12-09 03:37:19 -0500

    cards for table

--- src/Containers/Table/Table.js
+++ src/Containers/Table/Table.js
@@ -1,49 +1,73 @@
-import React from 'react'
+import React from "react";
 // import { provinces} from '../../Components/Api/Api'
 
+function Table({ basicData, provinces }) {
+  //get the data array from the main object
+  const lData = basicData.map((e) => e.data.data);
 
-function Table({ basicData, provinces}) {
+  //get the latest cases from the last array
+  let lastData = lData.map((e) => e[e.length - 1]);
 
-    //get the data array from the main object
-    const lData = basicData.map((e)=>e.data.data) 
+  //looped over all the object of lastData array and added name of each province.
+  for (let i = 0; i < lastData.length; i++) {
+    lastData[i].name = provinces[i].Name;
+  }
 
-    //get the latest cases from the last array
-    let lastData = lData.map((e)=>e[e.length -1])
-
-    //add name of the provinces to every lastData object, so it's easier to map
-    const newData = lastData.map((e,i)=>e['name'] = (provinces[i].Name))
-
-    return (
-
-            <table class="ui striped celled table unstackable seven column">
-                <thead>
-                    <tr>
-                        <th>Province</th>
-                        <th>Cases</th>
-                        <th>Deaths</th>
-                        <th>Active</th>
-                        <th>Hospitalizations</th>
-                        <th>Recoveries</th>
-                        <th>Tests</th>
-                    </tr>
-                </thead>
-                <tbody>  
-                    {lastData.map(data=>{
-                        return(
-                            <tr>
-                                <td data-label="Province">{data.name}</td>
-                                <td data-label="Cases">{data.total_cases}</td>
-                                <td data-label="Deaths">{data.total_fatalities}</td>
-                                <td data-label="Active">{data.total_cases - data.total_recoveries - data.total_fatalities}</td>
-                                <td data-label="Recoveries">{data.total_recoveries}</td>
-                                <td data-label="Hospitalizations">{data.total_hospitalizations}</td>
-                                <td data-label="Cases">{data.total_tests}</td>
-                            </tr>  
-                        )
-                    })}
-                </tbody>
-            </table>
-    )
+  return (
+    <div className="map__container">
+      <div className="ui card ">
+        <div className="content">
+          <div className="header">Canada COVID-19 Cases Provincial Map</div>
+        </div>
+        <div className="content">
+          <table class="ui striped celled table unstackable seven column">
+            <thead>
+              <tr>
+                <th>Province</th>
+                <th>Cases</th>
+                <th>Deaths</th>
+                <th>Active</th>
+                <th>Hospitalizations</th>
+                <th>Recoveries</th>
+                <th>Tests</th>
+              </tr>
+            </thead>
+            <tbody>
+              {lastData.map((data) => {
+                return (
+                  <tr>
+                    <td className="collapsing" data-label="Province">
+                      <b>{data.name}</b>
+                    </td>
+                    <td className="collapsing" data-label="Cases">
+                      {data.total_cases}
+                    </td>
+                    <td className="collapsing" data-label="Deaths">
+                      {data.total_fatalities}
+                    </td>
+                    <td className="collapsing" data-label="Active">
+                      {data.total_cases -
+                        data.total_recoveries -
+                        data.total_fatalities}
+                    </td>
+                    <td className="collapsing" data-label="Recoveries">
+                      {data.total_recoveries}
+                    </td>
+                    <td className="collapsing" data-label="Hospitalizations">
+                      {data.total_hospitalizations}
+                    </td>
+                    <td className="collapsing" data-label="Cases">
+                      {data.total_tests}
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Table
+export default Table;
