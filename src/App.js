9590bb6eb8ff950b8d24ed274adf0c16272dc746
prnvbirajdar <prnvbirajdar@gmail.com>
2020-12-30 19:08:21 -0500

    toml moved

--- src/App.js
+++ src/App.js
@@ -3,36 +3,36 @@ import { Dimmer, Loader } from "semantic-ui-react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import { provinces } from "./Components/Api/Api";
 
-import Nav from "./Components/Nav/Nav";
-import Footer from "./Components/Footer/Footer";
 import ScrollToTop from "./Components/SrollToTop";
 
+const Nav = React.lazy(() => import("./Components/Nav/Nav"));
 const Summary = React.lazy(() => import("./Components/Summary/Summary"));
 const Provinces = React.lazy(() => import("./Components/Provinces/Provinces"));
 const Regions = React.lazy(() => import("./Components/Regions/Regions"));
+const Footer = React.lazy(() => import("./Components/Footer/Footer"));
 
 function App() {
   return (
-    <Router>
-      <ScrollToTop />
-      <div className="App">
-        <Nav provinces={provinces} />
-        <Switch>
-          <React.Suspense
-            fallback={
-              <Dimmer active>
-                <Loader content="Loading" />
-              </Dimmer>
-            }
-          >
+    <React.Suspense
+      fallback={
+        <Dimmer active>
+          <Loader content="Loading" />
+        </Dimmer>
+      }
+    >
+      <Router>
+        <ScrollToTop />
+        <div className="App">
+          <Nav provinces={provinces} />
+          <Switch>
             <Route exact path="/" component={Summary} />
             <Route path="/province/:code" component={Provinces} />
             <Route path="/regions/:code" component={Regions} />
-          </React.Suspense>
-        </Switch>
-        <Footer />
-      </div>
-    </Router>
+          </Switch>
+          <Footer />
+        </div>
+      </Router>
+    </React.Suspense>
   );
 }
 
